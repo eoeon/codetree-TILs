fee568da109482b10dfdbd25d42fe513b4596b73
eoeon <158463294+eoeon@users.noreply.github.com>
2024-08-20 08:50:05 +0900

    Title: 정수 선언하고 뺄셈, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

--- 240820/정수 선언하고 뺄셈/define-numbers-and-substract.java
+++ 240820/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-            int a;
-            a = 5;
-            System.out.print(a);
+        System.out.print("97 - 13 = 84");
     }
 }
