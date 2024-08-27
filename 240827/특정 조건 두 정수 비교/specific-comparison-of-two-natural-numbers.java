6589e8c1dd7e3b841ab0f043d515344d59be3790
eoeon <158463294+eoeon@users.noreply.github.com>
2024-08-27 18:15:35 +0900

    Title: 특정 조건 두 정수 비교, Time: 193ms, Memory: 10MB, Status: Passed - CodeTree

--- 240827/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
+++ 240827/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -7,19 +7,19 @@ public class Main {
         int a = sc.nextInt(), b = sc.nextInt();
 
         if ( a < b ) {
-            System.out.print(1);
+            System.out.print("1");
         }
 
         else {
-            System.out.print(0);
+            System.out.print("0");
         }
 
         if ( a == b ) {
-            System.out.print(1);
+            System.out.print(" 1");
         }
 
         else {
-            System.out.print(0);
+            System.out.print(" 0");
         }
     }
 }
