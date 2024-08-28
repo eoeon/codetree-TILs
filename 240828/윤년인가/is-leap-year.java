a9181d2a9a13558d990a40f0970619c7d22158d7
eoeon <158463294+eoeon@users.noreply.github.com>
2024-08-28 19:45:19 +0900

    Title: 윤년인가, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240828/윤년인가/is-leap-year.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int y = sc.nextInt();
+
+        if ( y % 4 == 0 ) {
+            System.out.print("true");
+        }
+
+        else {
+            System.out.print("false")
+        }
+
+        
+    }
+}
