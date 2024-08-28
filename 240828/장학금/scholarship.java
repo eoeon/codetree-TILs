8f21c8f91034cc7041055cc9e303c8b3464e7759
eoeon <158463294+eoeon@users.noreply.github.com>
2024-08-28 13:22:26 +0900

    Title: 장학금, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240828/장학금/scholarship.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int mS = sc.nextInt(), mH = sc.nextInt();
+
+        if ( mS < 90 ) {
+            System.out.print("0");
+        }
+
+        else if ( mH >= 95 ) {
+            System.out.print("100000");
+        }
+
+        else{
+            System.out.print("50000");
+        }
+
+
+    }
+}
