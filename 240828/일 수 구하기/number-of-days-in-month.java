89d71c0ab1e8ac5f8a35623b95eb49d4e67e1458
eoeon <158463294+eoeon@users.noreply.github.com>
2024-08-28 20:37:48 +0900

    Title: 일 수 구하기, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240828/일 수 구하기/number-of-days-in-month.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if ( (n%2==1) && (n<=7) || (n%2==0) && (n>=8) ) {
+            System.out.print("31");
+        }
+
+        else {
+
+            if ( n==2 ) {
+                System.out.print("28");
+            }
+
+            else {
+                System.out.print("30");
+            }
+        }
+
+
+    }
+}
