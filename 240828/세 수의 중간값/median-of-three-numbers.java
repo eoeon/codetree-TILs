2543c09f69d49935d0ddcc88a04705c26a3b5625
eoeon <158463294+eoeon@users.noreply.github.com>
2024-08-28 09:19:55 +0900

    Title: 세 수의 중간값, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240828/세 수의 중간값/median-of-three-numbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+
+        if ( a < b && b < c ) {
+            System.out.print("1");
+        }
+
+        else {
+            System.out.print("0");
+        }
+    }
+}
