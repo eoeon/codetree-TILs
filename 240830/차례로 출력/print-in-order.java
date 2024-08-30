e68ea30123dec8f3962692bbe1f6beb0647e9f28
eoeon <158463294+eoeon@users.noreply.github.com>
2024-08-30 09:59:38 +0900

    Title: 차례로 출력, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240830/차례로 출력/print-in-order.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 5;
+        System.out.print(i);
+
+        
+        for ( i = 6; i < 18; i++ ) {
+            System.out.print( " " + i );
+        }
+    }
+}
