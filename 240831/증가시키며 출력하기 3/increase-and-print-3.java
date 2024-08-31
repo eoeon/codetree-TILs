a9dd2598736498c41112ab91e679538996cd012a
eoeon <158463294+eoeon@users.noreply.github.com>
2024-08-31 16:59:38 +0900

    Title: 증가시키며 출력하기 3, Time: 82ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240831/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 10;
+        
+        while ( i <= 26 ) {
+            System.out.print(i + " ");
+            i += 2;
+        }
+    }
+}
