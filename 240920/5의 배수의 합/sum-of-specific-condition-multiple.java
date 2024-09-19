8eeea34c66029f54c5bed833984c6eb0397f6b33
eoeon <158463294+eoeon@users.noreply.github.com>
2024-09-20 00:09:42 +0900

    Title: 5의 배수의 합, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240920/5의 배수의 합/sum-of-specific-condition-multiple.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int a = sc.nextInt(), b = sc.nextInt();
+        int sumVal = 0;
+
+        if ( a <= b ) {
+
+            for ( int i = a; i <= b; i++ ) {
+
+                if ( i % 5 == 0 ) {
+                    sumVal += i;
+                }
+            }
+        }
+        
+        else {
+
+            for ( int i = b; i <= a; i++ ) {
+
+                if ( i % 5 == 0 ) {
+                    sumVal += i;
+                }
+            }
+        }
+
+        System.out.print(sumVal);
+    }
+}
