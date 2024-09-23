626b42afd0735e8256202204cf365af426a675da
eoeon <158463294+eoeon@users.noreply.github.com>
2024-09-23 18:39:04 +0900

    Title: 친근하지 않은 수, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240923/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sumVal = 0;
+
+        for ( int i = 1; i <= n; i++ ) {
+
+            if ((i%2==0) || (i%3==0) || (i%5==0) ) {
+                continue;
+            }
+
+            else {
+                sumVal++;
+            }
+        }
+
+        System.out.print(sumVal);
+    }
+}
